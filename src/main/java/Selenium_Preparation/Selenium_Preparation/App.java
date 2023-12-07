5f76ec9b726680b72482b4fd1440073417007a88
SrinivasAddla <srinivas.addla@ratnaglobaltech.com>
2023-12-07 13:00:27 +0530

    first commit

---
+++ src/main/java/Selenium_Preparation/Selenium_Preparation/App.java
@@ -0,0 +1,13 @@
+package Selenium_Preparation.Selenium_Preparation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
